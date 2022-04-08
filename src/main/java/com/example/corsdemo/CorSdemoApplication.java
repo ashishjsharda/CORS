478d11f90600020d5606e3976cdaeb62179ee49a
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-08 06:37:50 -0500

    Add configuration

--- src/main/java/com/example/corsdemo/CorSdemoApplication.java
+++ src/main/java/com/example/corsdemo/CorSdemoApplication.java
@@ -2,12 +2,22 @@ package com.example.corsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CorSdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CorSdemoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configurerCors()
+    {
+
     }
 
 }
