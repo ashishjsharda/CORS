9b176497910e6038a3140ec400213931d8354063
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-08 06:35:22 -0500

    first commit

---
+++ src/test/java/com/example/corsdemo/CorSdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorSdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
